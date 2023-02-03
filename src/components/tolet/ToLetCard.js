5db5d5f3b31a19d2369b1a4993bf7936fb9f4eba
Uchchash346 <mru416@gmail.com>
2023-02-03 19:40:32 +0600

    setup axios and create tolet card by dev-mahmudur

---
+++ src/components/tolet/ToLetCard.js
@@ -0,0 +1,16 @@
+/* 
+File Created:3.2.2023
+Author: Mahmudur Rahman
+*/
+
+import React from 'react';
+
+const ToLetCard = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default ToLetCard;

73fd9b5e557441c0bd34b48083c8c90801ff2ef0
Uchchash346 <mru416@gmail.com>
2023-02-03 17:49:08 +0600

    create tolet json by dev-mahmudur

---
+++ src/components/tolet/ToLet.js
@@ -0,0 +1,18 @@
+/* 
+File Created:3.2.2023
+Author: Mahmudur Rahman
+*/
+
+// Import packages
+import React from 'react';
+
+// Main Code
+const ToLet = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ToLet;

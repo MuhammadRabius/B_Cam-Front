83f18c831f7445b18c87c0104bcd5e781af198ed
Uchchash346 <mru416@gmail.com>
2023-02-03 19:25:40 +0600

    setup tolet 1st part and install necessary packages by dev-mahmudur

--- src/components/tolet/ToLet.js
+++ src/components/tolet/ToLet.js
@@ -5,13 +5,24 @@ Author: Mahmudur Rahman
 
 // Import packages
 import React from 'react';
+import { IoIosArrowForward } from "react-icons/io";
 
 // Main Code
 const ToLet = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="grid grid-cols-2 container mx-auto">
+                <div>
+                    <p className="text-[#7A86F2] text-3xl px-5">To-LET</p>
+                </div>
+                <div>
+                    <p className="text-[#7A86F2] text-3xl px-5 text-right flex items-center">
+                        SEE MORE
+                        <IoIosArrowForward />
+                    </p>
+                </div>
+            </div>
+        </>
     );
 };
 
